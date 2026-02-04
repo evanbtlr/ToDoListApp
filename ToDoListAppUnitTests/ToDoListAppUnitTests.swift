5986c20826fc3297a498a9061285a0b17d051ac8
Evan Butler <evan.butler@icloud.com>
2026-02-04 21:57:12 +0700

    test: add unit and UI tests

---
+++ ToDoListAppUnitTests/ToDoListAppUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoListAppUnitTests.swift
+//  ToDoListAppUnitTests
+//
+//  Created by Evan Brother on 04.02.2026.
+//
+
+import Testing
+
+struct ToDoListAppUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
